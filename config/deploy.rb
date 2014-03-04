be6c2e6cebdcc8f3c3ad52c3370452a77fb4fece
NikSan <nikulinaleksandr@gmail.com>
2014-03-04 15:15:22 +0400

    upd

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,58 +43,12 @@ namespace :deploy do
     end
   end
 
-  after :finishing, 'deploy:cleanup'
-
-end
-=begin
-
-set :application,          'edem-design-shop'
-set :repo_url,             'git://github.com/niksan/edem-design-shop.git'
-set :scm,                  :git
-set :rails_env,            'production'
-set :rvm_ruby_string,      '2.1.0@rails4'
-set :deploy_to,            "/srv/htdocs/#{fetch(:application)}"
-set :unicorn_conf,         "#{fetch(:deploy_to)}/current/config/unicorn.rb"
-set :unicorn_pid,          "#{fetch(:deploy_to)}/shared/pids/unicorn.pid"
-set :bundle_cmd,           "cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)} do bundle exec install"
-set :migrate_cmd,          "cd #{fetch(:deploy_to)}/current; RAILS_ENV=#{fetch(:rails_env)} rvm use #{fetch(:rvm_ruby_string)} do bundle exec install"
-set :format,               :pretty
-set :log_level,            :info
-set :pty,                  true
-set :linked_files,         %w{config/database.yml}
-set :linked_dirs,          %w{bin log vendor/bundle public/system public/uploads}
-set :keep_releases,        10
-set :unicorn_start_cmd,    "(cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)} do bundle exec unicorn_rails -Dc #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)})"
-set :unicorn_stop_cmd,     "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
-
-namespace :deploy do
-
-  desc 'Start application'
-  task :start do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute fetch(:unicorn_start_cmd)
-    end
+  task :pipeline_precompile do
+    run "cd #{fetch(:deploy_to)}/current; rvm use #{fetch(:rvm_ruby_string)}; RAILS_ENV=production rake assets:precompile"
   end
-
-  desc 'Stop application'
-  task :stop do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute fetch(:unicorn_stop_cmd)
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute fetch(:unicorn_bundle_cmd)
-      execute fetch(:unicorn_migrate_cmd)
-      execute fetch(:unicorn_stop_cmd)
-      execute fetch(:unicorn_start_cmd)
-    end
-  end
+  
+  after :deploy, "deploy:update_code", "deploy:pipeline_precompile"
 
   after :finishing, 'deploy:cleanup'
 
 end
-
-=end
